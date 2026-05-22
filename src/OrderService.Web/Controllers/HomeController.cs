de29cb4ddc018325b780fc5e370c3765d7d0865d
Pushpavalli04 <88419811+Pushpavalli04@users.noreply.github.com>
2026-05-22 10:10:55 +0530

    feat: add home controller product listing

---
+++ src/OrderService.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Web.Models;
+using OrderService.Web.Services;
+
+namespace OrderService.Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IProductService _productService;
+
+    public HomeController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public IActionResult Index()
+    {
+        var products = _productService.GetAll();
+        return View(products);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
